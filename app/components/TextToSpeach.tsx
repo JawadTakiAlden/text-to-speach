16c9a95b2ef292bd09b79f5b646bfbd277a15160
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-07-21 13:30:18 +0300

    fix select of voices

--- app/components/TextToSpeach.tsx
+++ app/components/TextToSpeach.tsx
@@ -55,13 +55,13 @@ const TextToSpeech = () => {
         placeholder="Enter text to speak..."
       ></textarea>
       <select
-        value={voices[0].name}
+        value={voices[0]?.name}
     
       >
         {
-            voices.map((voice , i) => (
-                <option key={i} value={voice.name}>
-                    {`${voice.name} ${voice.lang}`}
+            voices?.map((voice , i) => (
+                <option key={i} value={voice?.name}>
+                    {`${voice?.name} ${voice?.lang}`}
                 </option>
             ))
         }
